c61db9df104977d314a4fb9cc21608c839f40443
Eduardo Souza <dudujv1910@gmail.com>
2025-02-20 18:18:47 -0300

    Create WHILE: Exemplo.java

---
+++ LaçoDeRepetição/WHILE: Exemplo.java
@@ -0,0 +1,15 @@
+public class MinhaClass_4 {
+    public static void main(String[] args) {
+
+        // Laço De Repetição no Java
+        // WHILE
+
+        int contador = 50;
+        int valor = 1;
+
+        while ( valor <= contador ){
+            System.out.println(valor);
+            valor = valor + 1;
+        }
+    }
+}
