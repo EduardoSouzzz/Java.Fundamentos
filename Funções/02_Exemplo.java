15d9ca26fa17738a0a025bba52b9639c7f674574
Eduardo Souza <dudujv1910@gmail.com>
2025-02-21 20:51:08 -0300

    02_Exemplo.java

---
+++ Funções/02_Exemplo.java
@@ -0,0 +1,12 @@
+public class Exemplo {
+  
+    // Função que soma dois números
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int resultado = soma(5, 3); // Chamando a função
+        System.out.println("Resultado da soma: " + resultado);
+    }
+}
