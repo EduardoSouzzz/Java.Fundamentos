95c8e48ba3d10b0c028e2a0b8f0b05fe778ab73c
Eduardo Souza <dudujv1910@gmail.com>
2025-02-24 17:39:48 -0300

    Create Exemplo.java

---
+++ OperadorTernário/Exemplo.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int idade = 18;
+
+        String deMaior = idade >= 18 ? "Sim, é de maior" : "Não, é de menor";
+        System.out.println(deMaior);
+    }
+}
