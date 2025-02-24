e52972b682d70e6df620b1a1d925be875190f736
Eduardo Souza <dudujv1910@gmail.com>
2025-02-24 18:55:11 -0300

    Create FOR: De trás p frente.java

---
+++ LaçoDeRepetição/FOR: De trás p frente.java
@@ -0,0 +1,10 @@
+public class MinhaClass_6 {
+    public static void main(String[] args) {
+
+        // De trás p frente;
+        
+        for (int i = 10; i > 0; i-=2 ) {
+            System.out.println(i);
+        }
+    }
+}
