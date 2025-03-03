e41877b5326b1174b7a687ed600aa39269059da2
Eduardo Souza <dudujv1910@gmail.com>
2025-03-03 16:01:05 -0300

    Nomes armazenados na Array

---
+++ Arrays/Strings.java
@@ -0,0 +1,10 @@
+public class MinhaClass {
+    public static void main(String[] args) {
+
+        String[] pessoas = { "Zé", "Pizzou", "Linnazin"};
+
+        System.out.println(pessoas[0]);
+        System.out.println(pessoas[1]);
+        System.out.println(pessoas[2]);
+    }
+}
