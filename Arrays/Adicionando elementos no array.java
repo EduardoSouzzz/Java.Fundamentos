b9e42eeb347a0aa6514a7741bcd7ddde0abcce7f
Eduardo Souza <dudujv1910@gmail.com>
2025-03-03 16:18:38 -0300

    Adicionando elementos no array

---
+++ Arrays/Adicionando elementos no array.java
@@ -0,0 +1,14 @@
+public class MinhaClass {
+    public static void main(String[] args) {
+
+        // Adicionando elementos no array
+
+        String[] telefones = new String[2];
+        telefones[0] = "11 95432-1245";
+        telefones[1] = "19 99123-9889";
+
+        System.out.println(telefones[0]);
+        System.out.println(telefones[1]);
+
+    }
+}
