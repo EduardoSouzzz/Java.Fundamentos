4de870c818affc5fd4539d70a10560a95ed88d05
Eduardo Souza <dudujv1910@gmail.com>
2025-03-12 18:18:22 -0300

    FOR: Números ímpar.java

--- LaçoDeRepetição/FOR: Números ímpar.java
+++ LaçoDeRepetição/FOR: Números ímpar.java
@@ -4,7 +4,6 @@ public class MinhaClass_5 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println();
         int X = scanner.nextInt();
 
         for (int i = 1; i <= X; i+=1) {
