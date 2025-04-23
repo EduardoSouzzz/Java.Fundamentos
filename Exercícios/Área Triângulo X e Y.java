a797fc156151e97ad04f315a3bff534b64a2bef9
Eduardo Souza <dudujv1910@gmail.com>
2025-04-23 16:28:33 -0300

    Área dos Triângulos X e Y

--- Exercícios/Área Triângulo X e Y.java
+++ Exercícios/Área Triângulo X e Y.java
@@ -6,26 +6,26 @@ public class MinhaClass_2 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite os lados do triangulo X: ");
-        double a = scanner.nextDouble();
-        double b = scanner.nextDouble();
-        double c = scanner.nextDouble();
+        double Xa = scanner.nextDouble();
+        double Xb = scanner.nextDouble();
+        double Xc = scanner.nextDouble();
 
         System.out.println("Digite os lados do triangulo Y: ");
-        double d = scanner.nextDouble();
-        double e = scanner.nextDouble();
-        double f = scanner.nextDouble();
+        double Ya = scanner.nextDouble();
+        double Yb = scanner.nextDouble();
+        double Yc = scanner.nextDouble();
 
 
         // area X;
-        double pX =  ( a + b + c ) / 2;
-        double areaDoTrianguloX = Math.sqrt(pX * (pX - a) * (pX - b) *
-                (pX - c));
+        double pX =  ( Xa + Xb + Xc ) / 2;
+        double areaDoTrianguloX = Math.sqrt(pX * (pX - Xa) * (pX - Xb) *
+                (pX - Xc));
         System.out.printf("Área do triangulo X: %.4f%n", areaDoTrianguloX);
 
         // area Y;
-        double pY = ( d + e + f) / 2;
-        double areaDoTrianguloY = Math.sqrt(pY * ( pY - d ) * (pY - e) *
-                (pY - f));
+        double pY = ( Ya + Yb + Yc) / 2;
+        double areaDoTrianguloY = Math.sqrt(pY * ( pY - Ya ) * (pY - Yb) *
+                (pY - Yc));
         System.out.printf("Área do triangulo Y: %.4f%n", areaDoTrianguloY);
 
 
