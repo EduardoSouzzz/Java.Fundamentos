7c42c23e21115eae7d5d52309937d25dc1290326
Eduardo Souza <dudujv1910@gmail.com>
2025-06-18 18:49:00 -0300

    FOR EACH: Exemplo

---
+++ LaçoDeRepetição/FOR EACH: Exemplo.java
@@ -0,0 +1,13 @@
+public class MinhaClass_8 {
+    public static void main(String[] args) {
+
+        String[] vect = new String[] {"Bob", "Zé", "Maria"};
+
+        // Laço for each
+
+        for (String nome : vect) {
+            System.out.println(nome);
+        }
+
+    }
+}
