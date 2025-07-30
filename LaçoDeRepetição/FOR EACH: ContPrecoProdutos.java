b294d826de0710d423d33cde78f876c854f23f4e
Eduardo Souza <dudujv1910@gmail.com>
2025-07-30 19:28:41 -0300

    FOR EACH: ContPrecoProdutos.java

---
+++ LaçoDeRepetição/FOR EACH: ContPrecoProdutos.java
@@ -0,0 +1,72 @@
+public class Produto {
+
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
+
+
+// MAIN:
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MC8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Produto> produtos = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Digite um produto " + (i + 1) + ": ");
+            String produtoo = sc.nextLine();
+
+            System.out.print("Digite o preço do produto " + (i + 1) + ": ");
+            double precoProd = sc.nextDouble();
+            sc.nextLine();
+
+            System.out.println();
+
+            Produto produto = new Produto(produtoo, precoProd);
+            produtos.add(produto);
+
+        }
+        System.out.println("Produtos Cadastrados: ");
+        for (Produto p : produtos) {
+            System.out.println("Nome: " + p.getNome() + " | Preço R$: " + p.getPreco());
+
+        }
+
+        System.out.println();
+
+        int cont = 0;
+        for (Produto contt : produtos) {
+            if (contt.getPreco() > 100) {
+                cont++;
+
+            }
+        }
+        System.out.println("Produtos maiores que R$ 100 reais: " + cont);
+        sc.close();
+    }
+}
